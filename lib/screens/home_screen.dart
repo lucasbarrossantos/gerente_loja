940c5ec9e1f95bf9567db65477d67acff9954aff
Lucas Barros <lucas-barros28@hotmail.com>
2019-09-28 11:39:01 -0300

    Sistema de páginas

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,8 +6,62 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  PageController _pageController;
+  int _page = 0;
+
+  @override
+  void initState() {
+    _pageController = PageController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.grey[850],
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+            canvasColor: Colors.pinkAccent,
+            primaryColor: Colors.white,
+            textTheme: Theme.of(context)
+                .textTheme
+                .copyWith(caption: TextStyle(color: Colors.white))),
+        child: BottomNavigationBar(
+            unselectedItemColor: Colors.pinkAccent[100],
+            showSelectedLabels: true,
+            currentIndex: _page,
+            onTap: (page) {
+              _pageController.animateToPage(page,
+                  duration: Duration(milliseconds: 500), curve: Curves.ease);
+            },
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.person), title: Text('Clientes')),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.shopping_cart), title: Text('Pedidos')),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.list), title: Text('Produtos')),
+            ]),
+      ),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (page) {
+          setState(() {
+            _page = page;
+          });
+        },
+        children: <Widget>[
+          Container(color: Colors.red),
+          Container(color: Colors.grey),
+          Container(color: Colors.white70),
+        ],
+      ),
+    );
   }
 }
