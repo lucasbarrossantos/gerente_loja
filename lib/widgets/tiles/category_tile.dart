8a6a3c76b2f04711cb97eb66cb971189d894793a
Lucas Barros <lucas-barros28@hotmail.com>
2019-10-01 21:57:47 -0300

    Exibindo a listagem dos produtos do Firebase

--- lib/widgets/tiles/category_tile.dart
+++ lib/widgets/tiles/category_tile.dart
@@ -1,7 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
 
 class CategoryTile extends StatelessWidget {
+  final controllerMasked = new MoneyMaskedTextController(
+      decimalSeparator: ',', thousandSeparator: '.');
   final DocumentSnapshot category;
 
   CategoryTile(this.category);
@@ -19,8 +22,44 @@ class CategoryTile extends StatelessWidget {
           title: Text(category.data['title'],
               style: TextStyle(
                   color: Colors.grey[850], fontWeight: FontWeight.w500)),
+          children: <Widget>[
+            FutureBuilder<QuerySnapshot>(
+                future: category.reference.collection('items').getDocuments(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData)
+                    return Container();
+                  else {
+                    return Column(
+                        children: snapshot.data.documents.map((doc) {
+                      return ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: NetworkImage(doc.data['images'][0]),
+                          backgroundColor: Colors.transparent,
+                        ),
+                        title: Text(doc.data['title']),
+                        trailing:
+                            Text('R\$${formatMoney(doc.data['price'] + 0.0)}'),
+                        onTap: () {},
+                      );
+                    }).toList()
+                          ..add(ListTile(
+                            leading: CircleAvatar(
+                              backgroundColor: Colors.transparent,
+                              child: Icon(Icons.add, color: Colors.pinkAccent),
+                            ),
+                            title: Text('Adicionar'),
+                            onTap: () {},
+                          )));
+                  }
+                })
+          ],
         ),
       ),
     );
+  }
+
+  String formatMoney(double value) {
+    controllerMasked.updateValue(value);
+    return controllerMasked.text;
   }
 }
